0861766b34c87133bf8c8afd1f58f53206d1b693
Namgyu Ho <itsnamgyu@gmail.com>
2022-06-27 18:06:49 +0000

    Allow any host in Django settings

--- mldemo/settings.py
+++ mldemo/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-iq_p9g&s((xizc%9l1%s(w1e!&u8z)4d3atud04w4_^o0l0zgh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "*"
+]
 
 
 # Application definition
